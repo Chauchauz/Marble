5ff782ab104d166f9291ccd38433bfd467cf59a7
kingofslim <79881938+kingofslim@users.noreply.github.com>
2021-05-18 11:11:08 +1000

    triangle

--- PROJECT/code/build.js
+++ PROJECT/code/build.js
@@ -54,6 +54,43 @@ function createCube()
     cube.position.y = 10;
 }
 
+PrismGeometry = function ( vertices, height ) {
+
+    var Shape = new THREE.Shape();
+
+    ( function f( ctx ) {
+
+        ctx.moveTo( vertices[0].x, vertices[0].y );
+        for (var i=1; i < vertices.length; i++) {
+            ctx.lineTo( vertices[i].x, vertices[i].y );
+        }
+        ctx.lineTo( vertices[0].x, vertices[0].y );
+
+    } )( Shape );
+
+    var settings = { };
+    settings.amount = height;
+    settings.bevelEnabled = false;
+    THREE.ExtrudeGeometry.call( this, Shape, settings );
+
+};
+
+PrismGeometry.prototype = Object.create( THREE.ExtrudeGeometry.prototype );
+
+var A = new THREE.Vector2( 0, 0 );
+var B = new THREE.Vector2( 30, 10 );
+var C = new THREE.Vector2( 20, 50 );
+
+var height = 12;                   
+var geometry = new PrismGeometry( [ A, B, C ], height ); 
+
+var material = new THREE.MeshPhongMaterial( { color: 0x00b2fc, specular: 0x00ffff, shininess: 20 } );
+
+var prism1 = new THREE.Mesh( geometry, material );
+prism1.rotation.x = -Math.PI  /  2;
+
+scene.add( prism1 );
+
 //Add all shapes to the scene
 function addShapes() {
     scene.add(floor);
--- PROJECT/code/run.js
+++ PROJECT/code/run.js
@@ -4,6 +4,7 @@ createFloor();
 createMarble();
 createCube();
 addShapes();
+PrismGeometry();
 document.addEventListener( 'keydown', onKeyDown, false );
 document.addEventListener( 'keyup', onKeyUp, false );
 animate();
